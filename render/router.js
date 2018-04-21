68f0409126b084e013545fdd7fdc2c15e5d6597e
严俊东 <1172482914@qq.com>
2018-04-21 09:52:07 +0800

    后台管理

--- render/router.js
+++ render/router.js
@@ -5,9 +5,19 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('index')
 })
+// 登入页
 router.get('/login', async (req, res) => {
     let data = await api.userInfo(req.headers)
     res.render('login', {data})
+})
+// 后台管理
+router.get('/dashboard', async (req, res) => {
+    let ret = await api.userInfo(req.headers)
+    if (ret.data) {
+        res.render('dashboard')
+    } else {
+        res.redirect('/login')
+    }
 })
 
 module.exports = router
